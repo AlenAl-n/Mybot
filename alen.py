d4aa1c0c9230eb74a502471fcb214f6ad4a72897
AlenAl-n <104082769+AlenAl-n@users.noreply.github.com>
2022-04-20 21:13:42 +0530

    Update alen.py

--- alen.py
+++ alen.py
@@ -5,9 +5,9 @@ import random
 
 Alen=Client(
     "Alen Bot",
-    bot_token="5102623936:AAGA7i5H0M80ZM6jqXiHWo4nu4fIA474fJA",
-    api_id="10429496",
-    api_hash="dc86c8623259c10c4fd1d1b7d8dab96f"
+    bot_token=" ",
+    api_id=" ",
+    api_hash=" "
 )
 
 
